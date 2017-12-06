96f231d2649ff888228ad8f007d215e2f32a3772
chechi <xchechi123x@yeah.net>
2017-12-06 23:28:36 +0800

    Recycle View layout

--- app/src/main/java/com/xiaolaogong/test/fragment/HomeFragment.java
+++ app/src/main/java/com/xiaolaogong/test/fragment/HomeFragment.java
@@ -3,6 +3,9 @@ package com.xiaolaogong.test.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +13,8 @@ import android.view.ViewGroup;
 import com.xiaolaogong.test.R;
 
 public class HomeFragment extends Fragment {
+
+    protected RecyclerView recyclerView;
 
     public HomeFragment() {
 
@@ -33,5 +38,21 @@ public class HomeFragment extends Fragment {
 
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initializeRecyclerView();
+    }
+
+    private void initializeRecyclerView() {
+        recyclerView = (RecyclerView) getView().findViewById(R.id.recycler_view);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) getView().findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setEnabled(true);
+    }
+
 
 }
